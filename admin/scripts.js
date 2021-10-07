ebc7f4769cba83c5805a9f6cc8fb52fb95c66f73
Jeffrey W Smith <45778516+SithProgrammer@users.noreply.github.com>
2021-10-06 20:27:00 -0500

    Add files via upload

---
+++ admin/scripts.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  $('#summernote').summernote({
+      height: 200
+  });
+    
+    
+$('#selectAllBoxes').click(function(event){
+                           
+if(this.checked){
+    
+    $('.checkBoxes').each(function(){
+        this.checked = true;
+    });
+}else{
+    $('.checkBoxes').each(function(){
+        this.checked = false;
+    });
+}
+});
+
+    
+    function loadUsersOnline() {
+    
+        
+        $.get("functions.php?onlineusers=result", function(data){
+            
+            $(".usersonline").text(data);
+            
+            
+    });
+}
+
+setInterval(function(){
+    
+    loadUsersOnline();
+},500);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
